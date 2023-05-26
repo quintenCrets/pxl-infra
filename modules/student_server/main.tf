e43d46a9e48d20f29ae283c42a661982d0c2ffd4
Bryan Honof <bryanhonof@gmail.com>
2023-05-26 14:56:49 +0200

    feat: add bryan by default to ssh_keys

--- modules/student_server/main.tf
+++ modules/student_server/main.tf
@@ -1,15 +1,15 @@
 resource "hcloud_ssh_key" "ssh_keys" {
   count      = length(var.ssh_keys)
-  name       = "SSH Key ${count.index} of ${var.student_name}"
+  name       = "SSH Key ${count.index} of ${lower(var.student_name)}"
   public_key = var.ssh_keys[count.index]
 }
 
 resource "hcloud_server" "server" {
-  name        = "server-of-${var.student_name}"
+  name        = "server-of-${lower(var.student_name)}"
   image       = var.image
   server_type = var.server_type
   location    = var.location
-  ssh_keys    = resource.hcloud_ssh_key.ssh_keys[*].id
+  ssh_keys    = concat(resource.hcloud_ssh_key.ssh_keys[*].id, "key-of-bryan" )
   public_net {
     ipv4_enabled = true
     ipv6_enabled = true
@@ -17,7 +17,7 @@ resource "hcloud_server" "server" {
 }
 
 resource "gandi_livedns_record" "a_record" {
-  name = "server-of-${var.student_name}.pxl"
+  name = "server-of-${lower(var.student_name)}.pxl"
   ttl = 10800
   type = "A"
   values = [ resource.hcloud_server.server.ipv4_address ]
@@ -25,7 +25,7 @@ resource "gandi_livedns_record" "a_record" {
 }
 
 resource "gandi_livedns_record" "aaaa_record" {
-  name = "server-of-${var.student_name}.pxl"
+  name = "server-of-${lower(var.student_name)}.pxl"
   ttl = 10800
   type = "AAAA"
   values = [ resource.hcloud_server.server.ipv6_address ]
