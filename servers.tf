9b785046f4a850604cdbb183c0471b8d2e96f5c8
Lucky234C <99822459+Lucky234C@users.noreply.github.com>
2023-05-26 09:36:57 +0200

    Lucas added public ssh key (#3)
    * aangepaste servers.tf
    
    * Added Quinten's pub key
    
    * Addes Quintens pub key
    
    ---------
    
    Signed-off-by: Bryan Honof <bryanhonof@gmail.com>
    Co-authored-by: Bryan Honof <bryanhonof@gmail.com>

--- servers.tf
+++ servers.tf
@@ -10,6 +10,9 @@ locals {
     # team3 = {
     #   ssh_keys = [ "ssh-ed25519 AAAA…", "ssh-ed25519 AAAA…" ]
     # }
+	group11 = {
+	  ssh_keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJwIhKqo5FscNIPvtgKYZDCQYvWRmke1ezipl0IYTjK/ 12200725@5CG21505JN", "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIHkB6VO39oyFwiQqZ88GwV11LBheK2ndJ1tKl9rByrE quintenmathijs@MacBook-Air-van-Quinten.local" ]
+	}
     team14 = {
       ssh_keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHxeiGtm/IHEAjC9mCGiw4JlYb3PVjMspiiKTMlGa5BC Jelle Claes@LAPTOP-4JCU1859", "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF+XlEVqXtXgcKqXswuHY/fCjWC5KyCX/8mqURWDwJqZ dries@LAPTOP-US63MDS9"]
     }
