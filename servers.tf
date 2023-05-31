e6c3a4c74f89ca40a4ca0c28191ec682b93375f3
Bryan Honof <bryanhonof@gmail.com>
2023-05-31 20:40:42 +0200

    refactor: remove demo and bryan's server

--- servers.tf
+++ servers.tf
@@ -41,8 +41,6 @@ locals {
     teamJnM = {
       ssh_keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHKcPhLMq6tXObUVTKWh0jIfosPFqyAhvsgDNQs3gNQH jaspe@MSI", "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEoqsX6vl6LKlNUmeHFJc6geEhe3K0RVO+nTrRWylpT6 jonas@DESKTOP-HD1COPB"]
     }
-    bryan = { ssh_keys = [] }
-    demo  = { ssh_keys = [] }
   }
 }
 
