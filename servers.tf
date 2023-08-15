3cd2ddc7cf741d629addca7e421834a55018ce1c
Bryan Honof <bryanhonof@gmail.com>
2023-08-15 17:59:27 +0200

    Thibe van orshaegen/trunk (#23)
    * Toevoeging ssh key
    
    * Toevoeging ssh key
    
    * Ssh key toevoegen
    
    * chore: fmt
    
    ---------
    
    Co-authored-by: ThibeVanOrshaegen <12200712@student.pxl.be>

--- servers.tf
+++ servers.tf
@@ -8,6 +8,11 @@ locals {
     #     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIuAcOS6oFayeYpmbe8xDPGKJels0OWq0NzrZxWCLX7d"
     #   ]
     # }
+    Thibe = {
+      ssh_keys = [
+        "SHA256:fv6PYIMthlswa0GPJebDQFyP+dZvLUwAMxEOQDnRpqg gebruiker@DESKTOP-BEAVFHB"
+      ]
+    }
   }
 }
 
