15f3938210d6d06c3781d099f96f0805e5126da7
Bryan Honof <bryan.honof@pxl.be>
2023-08-16 11:13:04 +0200

    fix: fix attribute name

--- servers.tf
+++ servers.tf
@@ -15,7 +15,7 @@ locals {
       ]
     }
     Thibe = {
-      ssh_key = [
+      ssh_keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA33dcN4/swspJZ9OPYLNWC2VMYu9J69hbod47kbnQ62 gebruiker@DESKTOP-BEAVFHB"
       ]
     }
