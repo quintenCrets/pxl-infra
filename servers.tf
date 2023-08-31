3bd5e1add90a842d4dbad2d925f05d6b20bb0d2a
Bryan Honof <bryan.honof@pxl.be>
2023-09-01 01:37:06 +0200

    chore: remove everyone

--- servers.tf
+++ servers.tf
@@ -8,28 +8,6 @@ locals {
     #     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIuAcOS6oFayeYpmbe8xDPGKJels0OWq0NzrZxWCLX7d"
     #   ]
     # }
-    Oliwier = {
-      ssh_keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIT7FdCTfgPdsQnBm/hRZtNFaaPCV7lPfse3EOn1lxsW oliwier@DESKTOP-DPCILQ3",
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGf+kcf1x/2nxnmIlbeqO5wqdCFN9tK/FZqzFUM1ginQ oliwi@olilaptop"
-      ]
-    }
-    xander = {
-      ssh_keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIALj15sjvwES0JDXVABoAo+h66gv+00TBMVkEfpy7B74 xande@Xander"
-
-      ]
-    }
-    Thibe = {
-      ssh_keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA33dcN4/swspJZ9OPYLNWC2VMYu9J69hbod47kbnQ62 gebruiker@DESKTOP-BEAVFHB"
-      ]
-    }
-    Runar = {
-      ssh_keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBKgm5n+eS/QiGrUHD6i2zFJRsfdEJtzhgJXLggQjuEz runar@runar"
-      ]
-    }
   }
 }
 
